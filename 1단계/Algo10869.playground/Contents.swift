e41d1724d9b3d305465fb2f1ffe5a25cebd539a5
92pino <jjb8382@gmail.com>
2019-09-01 18:39:32 +0900

    1단계 algorithm add

---
+++ 1단계/Algo10869.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+/*:
+ # 사칙연산 (#10869)
+ 
+ ## 문제
+ 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+ 
+ ## 예)
+ ### input:
+ 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+ 
+ 7 3
+ ### output:
+ 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+ 
+ 10
+ 4
+ 21
+ 2
+ 1
+ 
+*/
+
+let line = readLine() ?? ""
+let lineArr = line.split{$0 == " "}
+let a = Int(lineArr[0]) ?? 0
+let b = Int(lineArr[1]) ?? 0
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
---
+++ 1단계/Algo2557.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+/*:
+ # Hello World (#2557)
+ 
+ ## 문제
+ Hello World!를 출력하시오.
+ 
+ ## 예)
+ ### input:
+ 없음
+ ### output:
+ Hello World!를 출력하시오.
+ */
+
+print("Hello World!")
