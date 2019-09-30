b2921f286ca4ba80af67da4f296f48e70effd2cf
92pino <jjb8382@gmail.com>
2019-09-30 18:51:11 +0900

    code challenge 23 add

---
+++ CodeChallenge/Challenge23.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+/*:
+ # 2019.09.30 - Hackingwithswift
+ 
+ ## Challenge 23 : Integer disguised as string
+ __Difficulty__ : Tricky
+ 
+ Write a function that accepts a string and returns true if it contains only numbers, i.e. the digits 0 through 9.
+ 
+ ---
+ 
+ ### Sample input and output
+ - The input "01010101" should return true.
+ 
+ - The input "123456789" should return true.
+ 
+ - The letter "9223372036854775808" should return true.
+ 
+ - The letter "1.01" should return false; "." is not a number
+ */
+
+func challenge23(_ num: String) -> Bool {
+
+    var boolean = false
+    
+    if Int(num) != nil {
+        boolean = true
+    } else {
+        boolean = false
+    }
+    
+    return boolean
+}
+challenge23("123456789")
+
+
