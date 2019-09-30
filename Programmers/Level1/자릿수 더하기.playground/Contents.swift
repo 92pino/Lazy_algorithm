3f1501007ff32783410f3021f752f6c17459caf0
92pino <jjb8382@gmail.com>
2019-10-01 02:54:45 +0900

    자릿수 더하기 add

--- Programmers/Level1/자릿수 더하기.playground/Contents.swift
+++ Programmers/Level1/자릿수 더하기.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+/*:
+ # 문자열을 정수로 바꾸기
+ 
+ ## 문제
+ 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+ 예를들어 N = 123이면 1 + 2 + 3 = 6을 return하면 됩니다.
+ 
+ 제한 조건
+ - N의 범위 : 100,000,000 이하의 자연수
+ 
+ ## 예 1)
+ ### input:
+ 123
+ ### output:
+ 6
+ 
+ ## 예 2)
+ ### input:
+ 987
+ ### output:
+ 24
+ 
+*/
+
+func solution(_ n: Int) -> Int {
+    
+    let num = String(n).compactMap { $0.wholeNumberValue }
+    print(num)
+    
+    return num.reduce(0, +)
+    
+}
+solution(123)
