75f781a2e737f1599947385a585f2a96c3dd7b8c
92pino <jjb8382@gmail.com>
2019-10-02 00:52:13 +0900

    code refact regex use

--- CodeChallenge/Challenge24.playground/Contents.swift
+++ CodeChallenge/Challenge24.playground/Contents.swift
@@ -18,7 +18,7 @@ import UIKit
  - The string "h8ers" should return "8"
  */
 
-func challenge24(_ num: String) -> Int {
+func challenge24a(_ num: String) -> Int {
     var result = 0
     let values = num.components(separatedBy: CharacterSet.decimalDigits.inverted).flatMap { Int($0) }
     for i in values {
@@ -26,4 +26,32 @@ func challenge24(_ num: String) -> Int {
     }
     return result
 }
-challenge24("a10b20c30")
+challenge24a("a10b20c30")
+challenge24a("a1b2c3")
+challenge24a("h8ers")
+
+// 정규식으로 풀어보자
+func challenge24b(for regex: String, in text: String) -> [String] {
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+        print(results)
+        return results.map {
+            String(text[Range($0.range, in: text)!])
+        }
+    } catch let error {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
+    }
+}
+
+let string = "a10b20c30"
+
+var number = challenge24b(for: "[0-9]{1,2}", in: string)
+var resultb = 0
+
+for i in number {
+    resultb += Int(i)!
+}
+
+print(resultb)
