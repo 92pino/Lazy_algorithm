41182e1567feb3b57bba42275c57ac0c6dc3135e
92pino <jjb8382@gmail.com>
2019-10-06 03:24:14 +0900

    answer add

--- CodeChallenge/Challenge25.playground/Contents.swift
+++ CodeChallenge/Challenge25.playground/Contents.swift
@@ -41,3 +41,49 @@ func solution2(_ n: Int) -> Int {
 }
 
 solution2(15)
+
+// 정답
+func challengea(input: Int) -> Int {
+    guard input != 1 else { return 1 }
+    
+    var lowerBound = 0
+    var upperBound = 1 + input / 2
+    
+    while lowerBound + 1 < upperBound {
+        let middle = lowerBound + ((upperBound - lowerBound) / 2)
+        let middleSquared = middle * middle
+        
+        if middleSquared == input {
+            return middle
+        } else if middleSquared < input {
+            lowerBound = middle
+        } else {
+            upperBound = middle
+        }
+    }
+    
+    return lowerBound
+}
+challengea(input: 16777216)
+
+// 다른 풀이
+func challengeB(input: Int) -> Int {
+    var result = input / 2
+    var resultPlusOne = result + 1
+    
+    while !(result * result < input) || !(resultPlusOne * resultPlusOne > input) {
+        guard result * result != input else { return result }
+        guard resultPlusOne * resultPlusOne != input else { return resultPlusOne }
+        
+        if result * result > input {
+            result /= 2
+            resultPlusOne = result + 1
+        } else {
+            result += 1
+            resultPlusOne = result + 1
+        }
+    }
+    
+    return result
+}
+challengeB(input: 16777216)
