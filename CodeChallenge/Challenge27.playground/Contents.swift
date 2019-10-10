bfa151f84a108c9f8f7b0ab133cd8543a586e9b6
92pino <jjb8382@gmail.com>
2019-10-10 22:50:59 +0900

    challenge27 add

---
+++ CodeChallenge/Challenge27.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/*:
+ # 2019.10.10 - Hackingwithswift
+ 
+ ## Challenge 27 : Print last lines
+ __Difficulty__ : Easy
+ 
+ Write a function that accepts a filename on disk, then prints its last N lines in reverse order, all on a single line separated by commas.
+ 
+ ---
+ 
+ ### Sample input and output
+ - If asked to print the last 3 lines, your code should output "Twelfth Night, Othello, Macbeth".
+ - If asked to print the last 100 lines, your code should output "Twelfth Night, Othello, Macbeth, King Lear, Julius Caesar, Hamlet, Cymbeline, Coriolanus, Antony and Cleopatra".
+ - If asked to print the last 0 lines, your could should print nothing.
+ 
+*/
