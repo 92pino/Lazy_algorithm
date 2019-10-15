bdfa1571c7f3f4576ba54ca53e12f1350aae8a16
92pino <jjb8382@gmail.com>
2019-10-15 22:29:15 +0900

    challenge28 init

---
+++ CodeChallenge/Challenge28.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+/*:
+ # 2019.10.15 - Hackingwithswift
+ 
+ ## Challenge 28 : Log a message
+ __Difficulty__ : Easy
+ 
+ Write a logging function that accepts a path to a log file on disk as well as a new log message. Your function should open the log file (or create it if it does not already exist), then append the new message to the log along with the current time and date.
+ 
+ __Tip__ : It's important that you add line breaks along with each message, otherwise the log will just become jumbled.
+ 
+ ---
+ 
+ ### Sample input and output
+ - If the file does not exist, running your function should create it and save the new message.
+ - If it does exist, running your function should load the existing content and append the message to the end, along with suitable line breaking.
+ 
+*/
+
+func challenge28(fileName: String) {
+    
+    
+    
+}
+challenge28(fileName: "example")
